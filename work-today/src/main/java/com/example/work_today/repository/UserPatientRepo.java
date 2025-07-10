1b7bd93b5ad1467f2f188f1d1dbb84a7c7780449
nurubel1819 <rubel.18cse255@bsmrstu.edu.bd>
2025-07-10 09:25:33 +0600

    some projece move after inter repo

--- work-today/src/main/java/com/example/work_today/repository/UserPatientRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.work_today.repository;
-
-import com.example.work_today.entity.UserPatient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPatientRepo extends JpaRepository<UserPatient,Long> {
-
-}
